36ece494448ef9a138c993ae0a62836cbeb534e8
Shauryaditya13 <71700044+Shauryaditya13@users.noreply.github.com>
2020-11-21 22:30:29 +0530

    Add files via upload

---
+++ quiz_login.js
@@ -0,0 +1,8 @@
+function adduser() {
+    player1name=document.getElementById("player1_name").value;
+    player2name=document.getElementById("player2_name").value;
+    localStorage.setItem("player1namekey",player1name);
+    localStorage.setItem("player2namekey",player2name);
+    window.location="quiz_page.html";
+    
+}
